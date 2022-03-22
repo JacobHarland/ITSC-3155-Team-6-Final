bdf7b403cb2e6ca7fc88a33c9dd348b57b0fb844
Ryan Braswell <rbraswe1@uncc.edu>
2022-03-22 18:12:30 -0400

    First commit - added basic flask functionality

--- app.py
+++ app.py
@@ -5,3 +5,15 @@ app = Flask(__name__)
 @app.get('/')
 def index():
     return render_template('index.html')
+
+@app.get('/forum')
+def forum():
+    return render_template('forum.html')
+
+@app.get('/post')
+def post():
+    return render_template('post.html')
+
+@app.route('/post_form', methods=['GET','POST'])
+def post_form():
+    return render_template('post_form.html')

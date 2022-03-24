1f7d03281180188955bfc52f4d3e2574db8f01ee
cody liske <cody.liske01@gmail.com>
2022-03-23 20:12:27 -0400

    setting up venv and starting routes

--- app.py
+++ app.py
@@ -5,3 +5,17 @@ app = Flask(__name__)
 @app.get('/')
 def index():
     return render_template('index.html')
+
+@app.get('/profiles')
+def get_profiles():
+    for i in range(5):
+        return render_template('profile.html')
+
+@app.get('/profiles/<profile_id>')
+def get_profile_by_id( profile.profile_id ):
+    return render_template('profile.html')
+
+@app.post('/profiles')
+def add_profile():
+    name = request.form.get('name', '')
+    

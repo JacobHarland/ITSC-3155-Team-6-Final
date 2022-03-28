2c8988c8b7a44c2766f4bab73717f639e2a20b63
Caden Lalley <cadenlalley@gmail.com>
2022-03-28 17:27:16 -0400

    Working on fixing spacing issues on Home/about

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ app = Flask(__name__)
 
 @app.get('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', title="Home")
 
 @app.get("/about")
 def about():
-    return render_template('about.html')
+    return render_template('about.html', title="About Us")

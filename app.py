44baba5eb51bb015934dfe0642ec1b3e3a96ba21
Ryan Braswell <98066559+braswellry67@users.noreply.github.com>
2022-03-29 18:03:55 -0400

    app.py edited

--- app.py
+++ app.py
@@ -15,7 +15,6 @@ def index():
 def faq():
     return render_template('faq.html')
 
-
 @app.route('/signup', methods=['GET','POST'])
 def signup():
     form = SignupForm()
@@ -41,10 +40,6 @@ def login():
 def forum():
     return render_template('forum.html')
 
-@app.get('/post')
-def post():
-    return render_template('post.html')
-
 @app.get('/post_form')
 def post_form():
     return render_template('post_form.html')
@@ -54,25 +49,4 @@ def create_post():
     # TODO: Add post to DB
     # TODO: Redirect to post
     return redirect('/post')
-
-@app.route('/signup', methods=['GET','POST'])
-def signup():
-    form = SignupForm()
-    # displays a message if data was sent
-    if form.validate_on_submit():
-        flash(f'Account created for {form.username.data}!', 'success')
-        return redirect(url_for('index'))
-    return render_template('signup.html', form=form)
-
-@app.route('/login', methods=['GET','POST'])
-def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        # test data 
-        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
-            flash('You have been logged in!', 'success')
-            return redirect(url_for('index'))
-        else: 
-            flash('Login failed, please try again', 'danger')
-    return render_template('login.html', form=form)
 

3539dfa977e6dce828b07ef08ed1cdf89a3080d0
Ryan Braswell <rbraswe1@uncc.edu>
2022-04-06 18:11:45 -0400

    environment variables and connection string added

--- petpals/__init__.py
+++ petpals/__init__.py
@@ -1,5 +1,16 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+
+load_dotenv()
+db_user = os.getenv('dbuser', 'root')
+db_pass = os.getenv('dbpass')
+db_host = os.getenv('dbhost', 'localhost') #move this to wherever the "main" app file is
+db_port = os.getenv('dbport', 3306)
+db_name = os.getenv('dbname')
+
+connect = f'mysql://{db_user}:{db_pass}@{db_host}:{db_port}/{db_name}'
 
 app = Flask(__name__)
 # protects against modifying cookies and crosssite request forgery attacks

6d265c40cd7e50262eae8d2da50fb3b0ec416370
JacobHarland <geicojacob@gmail.com>
2022-04-06 16:37:05 -0400

    added flask-login

--- petpals/__init__.py
+++ petpals/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 # protects against modifying cookies and crosssite request forgery attacks
@@ -8,5 +9,6 @@ app.config['SECRET_KEY'] = '3b05adb71fc2d58cb11457a257f37b35'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
 
 from petpals import routes

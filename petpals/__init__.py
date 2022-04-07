d3fd9e02e21fd5bda510cb2cdd0237d6e8df0dbd
Ryan Braswell <rbraswe1@uncc.edu>
2022-04-06 20:53:44 -0400

    secret key made environment variable and database connection string used

--- petpals/__init__.py
+++ petpals/__init__.py
@@ -9,13 +9,15 @@ db_pass = os.getenv('dbpass')
 db_host = os.getenv('dbhost', 'localhost') #move this to wherever the "main" app file is
 db_port = os.getenv('dbport', 3306)
 db_name = os.getenv('dbname')
+secret_key = os.getenv('secretkey')
 
 connect = f'mysql://{db_user}:{db_pass}@{db_host}:{db_port}/{db_name}'
 
 app = Flask(__name__)
 # protects against modifying cookies and crosssite request forgery attacks
-app.config['SECRET_KEY'] = '3b05adb71fc2d58cb11457a257f37b35'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SECRET_KEY'] = secret_key
+app.config['SQLALCHEMY_DATABASE_URI'] = connect
 db = SQLAlchemy(app)
 
 from petpals import routes
+

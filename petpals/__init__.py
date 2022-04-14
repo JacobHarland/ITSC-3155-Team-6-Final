cf89f357119005b6958bbe345213340344713472
JacobHarland <geicojacob@gmail.com>
2022-04-13 22:56:46 -0400

    remove comment

--- petpals/__init__.py
+++ petpals/__init__.py
@@ -32,7 +32,6 @@ login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
-#app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USE_SSL'] = True
 app.config['MAIL_USERNAME'] = os.getenv('EMAIL_USER')
 app.config['MAIL_PASSWORD'] = os.getenv('EMAIL_PASS')

d13558da3151cf574da91f6e80fa207107b172a2
Ethan Pinto <ethanpintoa@gmail.com>
2022-04-20 16:58:44 -0400

    Reformat init file

--- petpals/__init__.py
+++ petpals/__init__.py
@@ -1,27 +1,36 @@
 import os
+
+from dotenv import load_dotenv
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
-from dotenv import load_dotenv
+from flask_sqlalchemy import SQLAlchemy
 
 load_dotenv()
 
 app = Flask(__name__)
 
-# Move to env
-# protects against modifying cookies and crosssite request forgery attacks
-app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+# SQLAlchemy
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+
+# Flask Secret Key
+# Protects against modifying cookies and crosssite request forgery attacks
+app.secret_key = os.getenv('SECRET_KEY')
+
+# Bcrypt
 bcrypt = Bcrypt(app)
+
+# Login Manager
 login_manager = LoginManager(app)
-# login_required needs to know where login route is.
-login_manager.login_view = 'login'
-# changes styling, info is bootstrap coloring
+# login route for login_required
+login_manager.login_view = 'auth_router.login'
+# Changes styling, info is bootstrap coloring
 login_manager.login_message_category = 'info'
+
+# Mail
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
 app.config['MAIL_USE_SSL'] = True
@@ -29,4 +38,4 @@ app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
 app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
 mail = Mail(app)
 
-from petpals import routes
+from petpals import routes as routes

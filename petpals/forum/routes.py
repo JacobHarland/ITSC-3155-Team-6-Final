d73ca02a6116b92f0376d5e276d5e604e768fd8e
cody liske <cody.liske01@gmail.com>
2022-04-29 17:08:16 -0400

    formatting with black

--- petpals/forum/routes.py
+++ petpals/forum/routes.py
@@ -2,17 +2,18 @@ from flask import Blueprint, render_template, request
 from petpals.models import Post, db
 
 router = Blueprint(
-    'forum_router', __name__, template_folder='templates', url_prefix="/forum"
+    "forum_router", __name__, template_folder="templates", url_prefix="/forum"
 )
 
 
-@router.get('')
+@router.get("")
 def forum():
     posts = Post.query.all()
-    return render_template('forum.html', posts=posts)
+    return render_template("forum.html", posts=posts)
 
-@router.post('/search')
+
+@router.post("/search")
 def search():
     search_param = request.form["search_param"]
     posts = db.session.query(Post).filter(Post.content.contains(search_param)).all()
-    return render_template('forum.html', posts=posts)
+    return render_template("forum.html", posts=posts)

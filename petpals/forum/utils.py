fb570f925c2fef82f6440e156420d8fe36a96984
Ethan Pinto <ethanpintoa@gmail.com>
2022-04-30 20:03:09 -0400

    Add markup for pet profile link
    Markup: @user[pet]

--- petpals/forum/utils.py
+++ petpals/forum/utils.py
@@ -5,7 +5,13 @@ from markupsafe import Markup
 
 
 def replace_markup(match: re.Match) -> str:
-    url = url_for("profile_router.profile_user", username=match[1])
+    if match['pet']:
+        url = url_for(
+            'profile_router.profile_pet', username=match['user'], pet_name=match['pet']
+        )
+    else:
+        url = url_for("profile_router.profile_user", username=match['user'])
+
     return f'<a href="{url}">{match[0]}</a>'
 
 
@@ -13,6 +19,12 @@ def utility_processor():
     def content_markup(content: str) -> Markup:
         safe_content = Markup.escape(content)
 
-        return Markup(re.sub(r'@([\w-]+)', replace_markup, safe_content))
+        return Markup(
+            re.sub(
+                r'@(?P<user>[\w-]+)(?:\[(?P<pet>[\w -]+)\])?',
+                replace_markup,
+                safe_content,
+            )
+        )
 
     return dict(content_markup=content_markup)

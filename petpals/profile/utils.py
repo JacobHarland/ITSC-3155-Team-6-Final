1fe2d549bcaf036104483562b24f6132d92e7921
Ethan Pinto <ethanpintoa@gmail.com>
2022-05-06 09:12:58 -0400

    Crop and resize profile pictures

--- petpals/profile/utils.py
+++ petpals/profile/utils.py
@@ -34,16 +34,21 @@ def save_profile_picture(picture_data: FileStorage, pet=None) -> str:
     f_ext = os.path.splitext(picture_data.filename)[1]
     filename = generate_image_name(f_ext, rel_path)
 
-    # Resize image
-    image = Image.open(picture_data)
-    # Check if it's too long vertically
-    if image.width * (29 / 23) < image.height:
-        size = (round(image.width * (290.0 / image.height)), 290)
-    else:
-        size = (230, round(image.height * (230.0 / image.width)))
-    image = image.resize(size, Image.LANCZOS)
+    with Image.open(picture_data) as image:
+        # Crop image to a 1:1 ratio
+        center = (image.width / 2.0, image.height / 2.0)
+        if image.height > image.width:
+            top = round(center[1] - (image.width / 2.0))
+            bottom = top + image.width
+            image = image.crop((0, top, image.width, bottom))
+        elif image.width > image.height:
+            left = round(center[0] - (image.height / 2.0))
+            right = left + image.height
+            image = image.crop((left, 0, right, image.height))
 
-    image.save(get_image_path(filename, rel_path), optimize=True)
+        image = image.resize((256, 256), Image.LANCZOS)
+
+        image.save(get_image_path(filename, rel_path), optimize=True)
 
     if profile.image_file != 'default.jpg':
         os.remove(get_image_path(profile.image_file, rel_path))
